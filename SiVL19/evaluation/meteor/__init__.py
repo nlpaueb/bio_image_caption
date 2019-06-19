d0cbfbb90b3bec089887c445e548762e6134d732
vasilikikou <vasilikvasilikikou@gmail.com>
2019-06-19 16:10:04 +0300

    Resolved comments

--- SiVL19/evaluation/meteor/__init__.py
+++ SiVL19/evaluation/meteor/__init__.py
@@ -2,25 +2,43 @@ import re
 from meteor import Meteor
 import sys
 import pandas as pd
+import tensorflow as tf
 
 scorer = Meteor()
+# clean for BioASQ https://github.com/nlpaueb/aueb-bioasq6
 bioclean = lambda t: re.sub('[.,?;*!%^&_+():-\[\]{}]', '', t.replace('"', '').replace('/', '').replace('\\', '').replace("'",'').strip().lower())
 
-def main(filepath):
+FLAGS = tf.app.flags.FLAGS
+
+tf.flags.DEFINE_string("filepath", "", "Path to JSON file")
+
+def main(argv):
+	"""
+	run as:
+	>> python __init__.py --filepath "path_to_json"
+	:param filepath: path to json file (json format: [{"id":"", "predicted_caption":"", "true_caption":""}]
+	:return: print METEOR score
+	"""
+
+	assert FLAGS.filepath, "--filepath is required"
+
 	gts = {}
 	res = {}
 
-	results = pd.read_json(filepath, orient="records", lines=True)
-	results.predicted_caption.apply(bioclean)
-	results.true_caption.apply(bioclean)
+	results = pd.read_json(FLAGS.filepath, orient="records")
+	results.predicted_caption = results.predicted_caption.apply(bioclean)
+	results.true_caption = results.true_caption.apply(bioclean)
 
-	for _, r in results.iterrows():
-		gts[r.id] = [r.true_caption]
-		res[r.id] = [r.predicted_caption]
+	gts = dict(zip(results.id, results.true_caption))
+	res = dict(zip(results.id, results.predicted_caption))
+
+	gts = {k: [v] for k, v in gts.items()}
+	res = {k: [v] for k, v in res.items()}
 
 	score, scores = scorer.compute_score(gts, res)
 
-	print(score)
+	print("Meteor score is %s" % score)
+	return score
 
 if __name__ == "__main__":
-	main(sys.argv[1])
+	tf.app.run()
